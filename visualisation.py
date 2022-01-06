1fc82e79d2edb575ca8d504068c8e060d77f81ec
svanburk <sarahvburk@gmail.com>
2022-01-06 20:47:52 +0100

    visualisatie

--- visualisation.py
+++ visualisation.py
@@ -2,33 +2,49 @@ from mesa.visualization.ModularVisualization import ModularServer
 from mesa.visualization.modules import CanvasGrid
 from main import RecyclingModel
 
+
 def agent_portrayal(agent):
     if agent.agent == "Household":
-        if agent.type == "Individual":
+        if agent.knowledge > 0.8:
             portrayal = {"Shape": "circle",
                          "Filled": "true",
                          "Layer": 0,
                          "Color": "red",
                          "r": 0.5}
+        elif agent.knowledge > 0.5:
+            portrayal = {"Shape": "circle",
+                         "Filled": "true",
+                         "Layer": 0,
+                         "Color": "orange",
+                         "r": 0.5}
         else:
             portrayal = {"Shape": "circle",
                          "Filled": "true",
                          "Layer": 0,
-                         "Color": "purple",
+                         "Color": "yellow",
                          "r": 0.5}
     elif agent.agent == "Municipality":
         portrayal = {"Shape": "rect",
                      "Filled": "true",
                      "Layer": 0,
-                     "Color": "blue",
+                     "Color": "black",
                      "w":1,
                      "h":1}
     elif agent.agent == "Company":
-        portrayal = {"Shape": "circle",
-                     "Filled": "true",
-                     "Layer": 0,
-                     "Color": "grey",
-                     "r": 2}
+        if agent.technology.throughput < 1000:
+            portrayal = {"Shape": "circle",
+                         "Filled": "true",
+                         "Layer": 0,
+                         "Color": "grey",
+                         "r": 1}
+        else:
+            big = agent.technology.throughput / 1000
+            portrayal = {"Shape": "circle",
+                         "Filled": "true",
+                         "Layer": 0,
+                         "Color": "grey",
+                         "r": big}
+
     return portrayal
 
 
